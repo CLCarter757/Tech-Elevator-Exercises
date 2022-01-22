44bce8dd9aa821b423a3184b5f24c67ab7a74db2
Clint Carter <CLCarter757@gmail.com>
2022-01-22 15:19:42 -0500

    Command-line Programs exercises complete.

--- module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/LinearConvert.java
+++ module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/LinearConvert.java
@@ -1,8 +1,40 @@
 package com.techelevator;
 
+import java.util.Scanner;
+import java.util.Locale;
+
 public class LinearConvert {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Please enter the length: ");
+		String userInput = input.nextLine();
+		double lengthInput = Double.parseDouble(userInput);
+
+		System.out.print("Is the measurement in (m)eter, or (f)feet? : ");
+		String unitType = input.nextLine();
+
+		System.out.print(lengthInput + unitType + " is " + conversion(lengthInput, unitType) + (unitType.startsWith("f") ? "m." : "f."));
+	}
+
+
+	public static double conversion(double lengthInput, String unitType) {
+		if (unitType.toLowerCase().startsWith("f")) {
+			double convertToMeters = lengthInput * 0.3048;
+			int finalMeter = (int)convertToMeters;
+			return finalMeter;
+		} else {
+			double convertToFeet = lengthInput * 3.2808399;
+			int finalFeet = (int)convertToFeet;
+			return finalFeet;
+	}
+
+
+
+
+
+
 
 	}
 
