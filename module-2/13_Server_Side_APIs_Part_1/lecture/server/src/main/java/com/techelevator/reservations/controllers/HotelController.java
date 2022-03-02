64b4cbf899f9630a256433f21f571c6d2e7cab20
Clint Carter <CLCarter757@gmail.com>
2022-03-02 16:37:36 -0500

    Server pt1 exercise

--- module-2/13_Server_Side_APIs_Part_1/lecture/server/src/main/java/com/techelevator/reservations/controllers/HotelController.java
+++ module-2/13_Server_Side_APIs_Part_1/lecture/server/src/main/java/com/techelevator/reservations/controllers/HotelController.java
@@ -5,10 +5,13 @@ import com.techelevator.reservations.dao.MemoryHotelDao;
 import com.techelevator.reservations.dao.MemoryReservationDao;
 import com.techelevator.reservations.dao.ReservationDao;
 import com.techelevator.reservations.model.Hotel;
+import com.techelevator.reservations.model.Reservation;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class HotelController {
 
     private HotelDao hotelDao;
@@ -23,10 +26,47 @@ public class HotelController {
      * Return All Hotels
      *
      * @return a list of all hotels in the system
+     *
+     * GET http://localhost:8080/hotels
      */
     @RequestMapping(path = "/hotels", method = RequestMethod.GET)
-    public List<Hotel> list() {
-        return hotelDao.list();
+//    public List<Hotel> list() {
+//        return hotelDao.list();
+//    }
+    public List<Hotel> filterAllHotels(@RequestParam(required = false) String city,
+                                       @RequestParam(required = false) String state) {
+        List<Hotel> allHotels = hotelDao.list();
+        List<Hotel> filteredHotels = new ArrayList<>();
+
+        if(city==null && state==null) {
+            return allHotels;
+        }
+        if (city != null && state != null) {
+            for(Hotel hotel : allHotels) {
+                if(hotel.getAddress().getCity().equals(city)
+                    && hotel.getAddress().getState().equals(state)) {
+                    filteredHotels.add(hotel);
+                }
+            }
+            return filteredHotels;
+        }
+        if (city != null && state==null) {
+            for(Hotel hotel : allHotels) {
+                if(hotel.getAddress().getCity().equals(city)) {
+                    filteredHotels.add(hotel);
+                }
+            }
+            return filteredHotels;
+        }
+        if (city == null && state != null) {
+            for(Hotel hotel : allHotels) {
+                if(hotel.getAddress().getState().equals(state)) {
+                    filteredHotels.add(hotel);
+                }
+            }
+            return filteredHotels;
+        }
+        return allHotels;
     }
 
     /**
@@ -39,5 +79,34 @@ public class HotelController {
     public Hotel get(@PathVariable int id) {
         return hotelDao.get(id);
     }
+
+    /**
+     * List all reservations
+     * @return
+     */
+    @RequestMapping(path = "/reservations", method = RequestMethod.GET)
+    public List<Reservation> listReservations () {
+        return reservationDao.findAll();
+    }
+
+    /**
+     * Return reservation by id
+     */
+    @RequestMapping(path = "/reservations/{id}", method = RequestMethod.GET)
+    public Reservation getReservation(@PathVariable int id) {
+        return reservationDao.get(id);
+    }
+
+    /**
+     * Create a new reservation
+     */
+    @RequestMapping(path = "/reservations", method = RequestMethod.POST)
+    //@PostMapping("/reservations") shorthand
+    public Reservation createReservation(@RequestBody Reservation reservation) {
+        return reservationDao.create(reservation, reservation.getHotelID());
+    }
+
+
+
 
 }
