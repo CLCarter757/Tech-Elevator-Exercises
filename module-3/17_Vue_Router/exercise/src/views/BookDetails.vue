5c353664076c0b9f12d96d6044e4f3d2c58eb402
Clint Carter <CLCarter757@gmail.com>
2022-04-05 15:49:35 -0400

    update

--- module-3/17_Vue_Router/exercise/src/views/BookDetails.vue
+++ module-3/17_Vue_Router/exercise/src/views/BookDetails.vue
@@ -13,9 +13,13 @@ export default {
     },
     computed: {
         book() {
-        return this.$store.state.books.find((book) => {
-         return book.isbn == this.$route.params.isbn;
-        });
+            if( this.$store.state.books.filter(book => book.isbn == this.$route.params.isbn).length > 0) {
+                return this.$store.state.books.find((book) => {
+                    return book.isbn == this.$route.params.isbn});
+            } else {
+                return this.$store.state.popularBooks.find((book) => {
+                    return book.isbn == this.$route.params.isbn});
+            }
         }      
     }
 }
